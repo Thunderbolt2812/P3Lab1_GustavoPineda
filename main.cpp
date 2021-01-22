6a6862fb6ea55b12bb65b95bbf06b9aae9e8385c
Thunderbolt2812 <Gustavopineda95@gmail.com>
2021-01-22 13:40:33 -0600

    Avance 1

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+	cout<<"Ingrese la opcion de ejercicio que desea ejecutar";
+	cout<<"1. Ejercicio Sumatoria";
+	cout<<"2. Ejercicio "
+	
+	return 0;
+}
